b7d14e34c20db82f8e6b3d8949505dacc8322331
Sam Hu <samyhhu@gmail.com>
2020-08-23 17:43:45 -0700

    wip

---
+++ replay_buffer.py
@@ -0,0 +1,14 @@
+class Game:
+  def __init__(self):
+    pass
+
+class ReplayBuffer:
+  def __init__(self):
+    self._games = []
+    pass
+
+  def save_game(self, game):
+    pass
+  
+  def sample_game(self, game);
+    pass
